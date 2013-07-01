b6053f98de5d430621d3e6fadf31c4d7a59a02f1
tararoskell <tara@troskell.demon.co.uk>
2013-07-01 17:34:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mocktwitter::Application.config.secret_token = '183671f07d83e95a425eb84886d0187fdfa154397a8e834fd65d01601e35c349b6a18372df9f7e7821127315bfa938c6b62bf25061bf9fa1f4bc7cef0eb79ab4'
